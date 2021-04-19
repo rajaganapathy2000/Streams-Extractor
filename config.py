85d470764fbe651887a090b4c6c0d38af45f0093
Raja Ganapathy <prajaganapathy771@gmail.com>
2021-04-19 23:06:12 +0530

    Uploaded

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1723222143:AAGJ6h8QDMLukcpgigA-ATM9gk9Wzpcaenk")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "2842684"))
+    API_HASH = os.environ.get("API_HASH", "795e3027e6144207a130966d96a93f00")
     
